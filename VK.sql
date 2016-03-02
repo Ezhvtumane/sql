be6f5994468bea3a973f616bb94874d513737ddb
Ezhvtumane <gogaopel@gmail.com>
2016-03-02 15:47:01 +0300

    Финальная версия с ак

--- VK.sql
+++ VK.sql
@@ -1,9 +1,9 @@
-		select data, ctxtotd, kolvoib,COUNT(*) kolvovk,
+		select data, ctxtotd, kolvosl,COUNT(*) kolvovk,
 				SUM(p2_1_sum), SUM(p4_1_sum), SUM(p5_1_sum), SUM(p10_1_sum), SUM(p14_1_sum), SUM(fansumm1),p16,p17
 			from
 				(
 				select 
-					'Month' data, ib.ctxtOtd,kolvoib, 
+					':PeriodBegin '+'-'+' :PeriodEnd' data, vk.ctxtOtd, case when kolvoib IS NULL then kolvoak else kolvoib end kolvosl, 
 					case p2_1 when 0.5 then 1
 					when 0 then 1 
 					else 0 end p2_1_sum,
@@ -23,31 +23,38 @@
 					(
 					SELECT STUFF(
 									(SELECT ', ' + place16 
-									FROM QuaSeqControl where ctxtotd=ib.ctxtOtd
+									FROM QuaSeqControl where ctxtotd=vk.ctxtOtd--(case when ib.ctxtOtd is null then ak.ctxtOtd else ib.ctxtOtd end)
 									FOR XML PATH(''), TYPE).value('.', 'VARCHAR(MAX)'), 1, 2, ''
 								) 
 					) p16,
 					(
 					SELECT STUFF(
 									(SELECT ', ' + place17 
-									FROM QuaSeqControl where ctxtotd=ib.ctxtOtd
+									FROM QuaSeqControl where ctxtotd=vk.ctxtOtd--(case when ib.ctxtOtd is null then ak.ctxtOtd else ib.ctxtOtd end)
 									FOR XML PATH(''), TYPE).value('.', 'VARCHAR(MAX)'), 1, 2, ''
 								) 
 					) p17
 				from
 						/*Информация из листов ВК*/
 					(
-						select datdatevizit, ctxtotd, p2_1,p4_1,p5_1,p10_1,p14_1,fansumm1--,place16,place17
+						select datdatevizit, ctxtotd, p2_1,p4_1,p5_1,p10_1,p14_1,fansumm1,place16,place17
 						from QuaSeqControl 
-						where datdatevizit>='01.10.2015' and (ctxtotd not like '%поликл%' and ctxtotd not like '%маркет%')
+						where datdatevizit between ':PeriodBegin' and ':PeriodEnd'/* and (ctxtotd not like '%поликл%' and ctxtotd not like '%маркет%')*/
 					) vk
 						left join
 						/*Истории переданные в статистику*/
 					(
 						select ctxtOtd,COUNT(*) kolvoib
 						from IstorTable
-						where PODPIS<>'Не указана' and (CSType='1' or CSType='3') and datdatevizit>='01.10.2015'
+						where PODPIS<>'Не указана' and (CSType='1' or CSType='3') and datdatevizit between ':PeriodBegin' and ':PeriodEnd'
 						group by ctxtOtd
 					) ib on vk.ctxtotd = ib.ctxtOtd
+						left join
+						/*Стат талоны*/
+					(
+						select ctxtOtd, COUNT(*) kolvoak from StatTal4
+						where datdatevizit between ':PeriodBegin' and ':PeriodEnd'
+						group by ctxtOtd
+					) ak on vk.ctxtOtd=ak.ctxtOtd
 				) t
-						group by data, ctxtotd, kolvoib,p16,p17
+						group by data, ctxtotd, kolvosl,p16,p17
